2cb4d5f0f708e8d71f1ca984d87ef335b417f21f
Neetesh NG <neeteshng15@gmail.com>
2023-10-11 18:10:52 +0530

    Used useNavigate to navigate to the URL

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Card({data, numPosts}) {
+function Card({data, numPosts, onCardClick}) {
   return (
     <div>
-      <Link to={`/userdetails/${data.id}`} className='card-container'>
+      <Link to={`/userdetails/${data.id}`} className='card-container' onClick={onCardClick}>
         <div className="card">
             <p>{data.username}</p>
             <p>Post : {numPosts}</p>
--- src/Components/Directory.js
+++ src/Components/Directory.js
@@ -1,12 +1,158 @@
 import React from 'react'
 import Card from './Card'
-import userData from './userData.json'
+import { useNavigate } from 'react-router-dom'
 
 function Directory() {
+  const navigate = useNavigate();
+
+  const data = [
+    {
+      "id": 1,
+      "name": "Neetesh Gupta",
+      "address": "Ashoknagar",
+      "phone": "+91-7000835163",
+      "username": "Neetesh",
+      "posts": [
+        {"id" : 1, "content" : "A Content 1", "url" : "https://picsum.photos/200/300"},
+        {"id" : 2, "content" : "A Content 2", "url" : "https://picsum.photos/200/300"},
+        {"id" : 3, "content" : "A Content 3", "url" : "https://picsum.photos/200/300"},
+        {"id" : 4, "content" : "A Content 4", "url" : "https://picsum.photos/200/300"},
+        {"id" : 5, "content" : "A Content 5", "url" : "https://picsum.photos/200/300"}
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Shri Yadav",
+      "address": "Banglore",
+      "phone": "+91-9815421421",
+      "username": "Shri",
+      "posts": [
+        {"id" : 1, "content" : "A Content 1"},
+        {"id" : 2, "content" : "A Content 2"},
+        {"id" : 3, "content" : "A Content 3"},
+        {"id" : 4, "content" : "A Content 4"},
+        {"id" : 5, "content" : "A Content 5"},
+        {"id" : 6, "content" : "A Content 6"},
+        {"id" : 7, "content" : "A Content 7"},
+        {"id" : 8, "content" : "A Content 8"},
+        {"id" : 9, "content" : "A Content 9"},
+        {"id" : 10, "content" : "A Content 10"}
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Rahul Khanna",
+      "address": "Mumbai",
+      "phone": "+91-8741216598",
+      "username": "Rahul",
+      "posts": [
+        {"id" : 1, "content" : "A Content 1"},
+        {"id" : 2, "content" : "A Content 2"},
+        {"id" : 3, "content" : "A Content 3"},
+        {"id" : 4, "content" : "A Content 4"},
+        {"id" : 5, "content" : "A Content 5"},
+        {"id" : 6, "content" : "A Content 6"}
+      ]
+    },
+    {
+      "id": 4,
+      "name": "Rohit Mishra",
+      "address": "Bhopal",
+      "phone": "+91-9875110647",
+      "username": "Rohit",
+      "posts": [
+        {"id" : 1, "content" : "A Content 1"},
+        {"id" : 2, "content" : "A Content 2"},
+        {"id" : 3, "content" : "A Content 3"},
+        {"id" : 4, "content" : "A Content 4"},
+        {"id" : 5, "content" : "A Content 5"},
+        {"id" : 6, "content" : "A Content 6"},
+        {"id" : 7, "content" : "A Content 7"},
+        {"id" : 8, "content" : "A Content 8"}
+      ]
+    },
+    {
+      "id": 5,
+      "name": "Deepak Sahu",
+      "address": "Ujjain",
+      "phone": "+91-9874556252",
+      "username": "Deepak",
+      "posts": [
+        {"id" : 1, "content" : "A Content 1"},
+        {"id" : 2, "content" : "A Content 2"},
+        {"id" : 3, "content" : "A Content 3"},
+        {"id" : 4, "content" : "A Content 4"},
+        {"id" : 5, "content" : "A Content 5"},
+        {"id" : 6, "content" : "A Content 6"},
+        {"id" : 7, "content" : "A Content 7"},
+        {"id" : 8, "content" : "A Content 8"},
+        {"id" : 9, "content" : "A Content 9"}
+      ]
+    },
+    {
+      "id": 6,
+      "name": "Deepti Vishw",
+      "address": "Shimla",
+      "phone": "+91-9787254544",
+      "username": "Deepti",
+      "posts": [
+        {"id" : 1, "content" : "A Content 1"},
+        {"id" : 2, "content" : "A Content 2"},
+        {"id" : 3, "content" : "A Content 3"},
+        {"id" : 4, "content" : "A Content 4"},
+        {"id" : 5, "content" : "A Content 5"},
+        {"id" : 6, "content" : "A Content 6"},
+        {"id" : 7, "content" : "A Content 7"}
+      ]
+    },
+    {
+      "id": 7,
+      "name": "Shruti Chouhan",
+      "address": "Kashmir",
+      "phone": "+91-9754627546",
+      "username": "Shruti",
+      "posts": [
+        {"id" : 1, "content" : "A Content 1"},
+        {"id" : 2, "content" : "A Content 2"},
+        {"id" : 3, "content" : "A Content 3"},
+        {"id" : 4, "content" : "A Content 4"}
+      ]
+    },
+    {
+      "id": 8,
+      "name": "Kapil Bhati",
+      "address": "Kota",
+      "phone": "+91-9724254211",
+      "username": "Kapil",
+      "posts": [
+        {"id" : 1, "content" : "A Content 1"},
+        {"id" : 2, "content" : "A Content 2"}
+      ]
+    },
+    {
+      "id": 9,
+      "name": "Piyush Sharma",
+      "address": "Indore",
+      "phone": "+91-8925484132",
+      "username": "Piyush",
+      "posts": [
+        {"id" : 1, "content" : "A Content 1"}
+      ]
+    }
+  ]
+  
+  const openUserDetails = (id) => {
+    navigate(`/userdetails/${id}`)
+  }
+
   return (
     <div>
-      {userData.map(user => 
-        <Card key={user.id} data={user} numPosts={user.posts.length}/>
+      {data.map(user => 
+        <Card 
+          key={user.id} 
+          data={user} 
+          numPosts={user.posts.length} 
+          onCardClick={() => openUserDetails(user.id)}/>
       )}
     </div>
   )     
