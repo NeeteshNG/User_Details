31b586398994922aed2c726d290460a48a00b62f
Neetesh NG <neeteshng15@gmail.com>
2023-10-11 11:43:09 +0530

    Started

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li class="center"><a href="#">Directory</a></li>
+        </ul>
+    </>
+  )
+}
+
+export default Header
