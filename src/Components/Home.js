c6039baf82330031e2527c4fcf3b6f8e99dcd493
Neetesh NG <neeteshng15@gmail.com>
2023-10-12 11:18:36 +0530

    Made Home Page Looks Good

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      <h1>This is Home</h1>
+    <div className='home-container'>
+      <h1 className='mint'>This is Home</h1>
     </div>
   )
 }
