0afec89b60f202028576a493d6e9674b03351ec3
Neetesh NG <neeteshng15@gmail.com>
2023-10-12 12:41:28 +0530

    Make the Login to Work as Expected

--- src/App.js
+++ src/App.js
@@ -6,6 +6,143 @@ import Home from './Components/Home';
 import UserDetails from './Components/User';
 import LoginPage from './Components/LoginPage';
 
+const data = [
+  {
+    "id": 1,
+    "name": "Neetesh Gupta",
+    "address": "Ashoknagar",
+    "phone": "+91-7000835163",
+    "username": "Neetesh",
+    "password": "neetesh@123",
+    "posts": [
+      {"id" : 1, "content" : "A Content 1", "url" : "https://picsum.photos/200/300"},
+      {"id" : 2, "content" : "A Content 2", "url" : "https://picsum.photos/200/300"},
+      {"id" : 3, "content" : "A Content 3", "url" : "https://picsum.photos/200/300"},
+      {"id" : 4, "content" : "A Content 4", "url" : "https://picsum.photos/200/300"},
+      {"id" : 5, "content" : "A Content 5", "url" : "https://picsum.photos/200/300"}
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Shri Yadav",
+    "address": "Banglore",
+    "phone": "+91-9815421421",
+    "username": "Shri",
+    "posts": [
+      {"id" : 1, "content" : "A Content 1", "url" : "https://picsum.photos/200"},
+      {"id" : 2, "content" : "A Content 2", "url" : "https://picsum.photos/200"},
+      {"id" : 3, "content" : "A Content 3", "url" : "https://picsum.photos/200"},
+      {"id" : 4, "content" : "A Content 4", "url" : "https://picsum.photos/200"},
+      {"id" : 5, "content" : "A Content 5", "url" : "https://picsum.photos/200"},
+      {"id" : 6, "content" : "A Content 6", "url" : "https://picsum.photos/200"},
+      {"id" : 7, "content" : "A Content 7", "url" : "https://picsum.photos/200"},
+      {"id" : 8, "content" : "A Content 8", "url" : "https://picsum.photos/200"},
+      {"id" : 9, "content" : "A Content 9", "url" : "https://picsum.photos/200"},
+      {"id" : 10, "content" : "A Content 10", "url" : "https://picsum.photos/200"}
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Rahul Khanna",
+    "address": "Mumbai",
+    "phone": "+91-8741216598",
+    "username": "Rahul",
+    "posts": [
+      {"id" : 1, "content" : "A Content 1", "url" : "https://source.unsplash.com/random/200x200?sig=1"},
+      {"id" : 2, "content" : "A Content 2", "url" : "https://source.unsplash.com/random/200x200?sig=1"},
+      {"id" : 3, "content" : "A Content 3", "url" : "https://source.unsplash.com/random/200x200?sig=1"},
+      {"id" : 4, "content" : "A Content 4", "url" : "https://source.unsplash.com/random/200x200?sig=1"},
+      {"id" : 5, "content" : "A Content 5", "url" : "https://source.unsplash.com/random/200x200?sig=1"},
+      {"id" : 6, "content" : "A Content 6", "url" : "https://source.unsplash.com/random/200x200?sig=1"}
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Rohit Mishra",
+    "address": "Bhopal",
+    "phone": "+91-9875110647",
+    "username": "Rohit",
+    "posts": [
+      {"id" : 1, "content" : "A Content 1", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 2, "content" : "A Content 2", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 3, "content" : "A Content 3", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 4, "content" : "A Content 4", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 5, "content" : "A Content 5", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 6, "content" : "A Content 6", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 7, "content" : "A Content 7", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 8, "content" : "A Content 8", "url" : "https://loremflickr.com/320/240"}
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Deepak Sahu",
+    "address": "Ujjain",
+    "phone": "+91-9874556252",
+    "username": "Deepak",
+    "posts": [
+      {"id" : 1, "content" : "A Content 1", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 2, "content" : "A Content 2", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 3, "content" : "A Content 3", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 4, "content" : "A Content 4", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 5, "content" : "A Content 5", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 6, "content" : "A Content 6", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 7, "content" : "A Content 7", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 8, "content" : "A Content 8", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 9, "content" : "A Content 9", "url" : "https://loremflickr.com/320/240"}
+    ]
+  },
+  {
+    "id": 6,
+    "name": "Deepti Vishw",
+    "address": "Shimla",
+    "phone": "+91-9787254544",
+    "username": "Deepti",
+    "posts": [
+      {"id" : 1, "content" : "A Content 1", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 2, "content" : "A Content 2", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 3, "content" : "A Content 3", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 4, "content" : "A Content 4", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 5, "content" : "A Content 5", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 6, "content" : "A Content 6", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 7, "content" : "A Content 7", "url" : "https://loremflickr.com/320/240"}
+    ]
+  },
+  {
+    "id": 7,
+    "name": "Shruti Chouhan",
+    "address": "Kashmir",
+    "phone": "+91-9754627546",
+    "username": "Shruti",
+    "posts": [
+      {"id" : 1, "content" : "A Content 1", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 2, "content" : "A Content 2", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 3, "content" : "A Content 3", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 4, "content" : "A Content 4", "url" : "https://loremflickr.com/320/240"}
+    ]
+  },
+  {
+    "id": 8,
+    "name": "Kapil Bhati",
+    "address": "Kota",
+    "phone": "+91-9724254211",
+    "username": "Kapil",
+    "posts": [
+      {"id" : 1, "content" : "A Content 1", "url" : "https://loremflickr.com/320/240"},
+      {"id" : 2, "content" : "A Content 2", "url" : "https://loremflickr.com/320/240"}
+    ]
+  },
+  {
+    "id": 9,
+    "name": "Piyush Sharma",
+    "address": "Indore",
+    "phone": "+91-8925484132",
+    "username": "Piyush",
+    "posts": [
+      {"id" : 1, "content" : "A Content 1", "url" : "https://loremflickr.com/320/240"}
+    ]
+  }
+]
+
 function App() {
   return (
     <BrowserRouter>
@@ -14,9 +151,9 @@ function App() {
 
         <Routes>
           <Route path='/' element={<Home/>}></Route>
-          <Route path='/directory' element={<Directory/>}></Route>
-          <Route path='/userdetails/:id' element={<UserDetails/>}></Route>
-          <Route path='/loginpage' element={<LoginPage/>}></Route>
+          <Route path='/directory' element={<Directory data={data}/>}></Route>
+          <Route path='/userdetails/:id' element={<UserDetails data={data}/>}></Route>
+          <Route path='/loginpage' element={<LoginPage data={data}/>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,42 +1,72 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react';
 
-function LoginPage() {
-  return (
-    <div className='login-container'>
-        <div class="signin"> 
+function LoginPage( {data} ) {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-            <div class="content"> 
+    const handleLogin = () => {
+        const user = data.find(
+            (user) => user.username == username && user.password == password
+        );
 
-                <h2>Sign In</h2> 
+        if (user){
+            navigate('/directory');
+        }
+        else{
+            setError('Invalid Username or Password.');
+        }
+    }
 
-                <div class="form"> 
+    
+    return (
+        <div className='login-container'>
+            <div class="signin"> 
 
-                    <div class="inputBox"> 
+                <div class="content"> 
 
-                    <input type="text" required/> <i>Username</i> 
+                    <h2>Log In</h2> 
 
-                    </div> 
+                    <div class="form"> 
 
-                    <div class="inputBox"> 
+                        <div class="inputBox"> 
 
-                        <input type="password" required/> <i>Password</i> 
+                        <input 
+                            type="text" 
+                            value={username} 
+                            onChange={(e) => setUsername(e.target.value)}
+                            required/> <i>Username</i> 
+                        </div> 
 
-                    </div> 
+                        <div class="inputBox"> 
 
-                    <div class="inputBox"> 
+                            <input 
+                                type="password" 
+                                value={password} 
+                                onChange={(e) => setPassword(e.target.value)}
+                                required/> <i>Password</i> 
 
-                        <input type="submit" value="Login"/> 
+                        </div> 
+
+                        {error && <p>{error}</p>}
+
+                        <div class="inputBox" onClick={handleLogin}> 
+
+                            <input type="submit" value="Login"/> 
+
+                        </div> 
 
                     </div> 
 
                 </div> 
 
-            </div> 
+        </div> 
+        </div>
 
-    </div> 
-    </div>
-
-  )
+    )
 }
 
 export default LoginPage
