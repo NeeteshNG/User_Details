53228ead19edc1b9bfca8dfb7b8e92b37b1cb554
Neetesh NG <neeteshng15@gmail.com>
2023-11-02 10:33:34 +0530

    Added button Git Hub Repositoty

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,39 +1,50 @@
-import { Component } from 'react';
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
+import { Component } from "react";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
   render() {
     const handleLogOut = () => {
       this.props.setLoggedIn(false);
-      if (this.props.history){
-        this.props.history.push('/loginpage');
-      }
-      else{
-        console.error("Having History Error.")
+      if (this.props.history) {
+        this.props.history.push("/loginpage");
+      } else {
+        console.error("Having History Error.");
       }
     };
 
     return (
       <>
         <ul className="nav-links">
-          <NavLink to='/'>
+          <NavLink to="/">
             <li>
-              <div id='a'>Home</div>
+              <div id="a">Home</div>
             </li>
           </NavLink>
+          <div className="ul_git">
+            <div className="il_git">
+              <a
+                href="https://github.com/NeeteshNG/MyTaskBestPeers_2"
+                rel="noreferrer"
+                target="_blank"
+                title="Git Repo"
+                className="git-click"
+              >
+                <i className="fab fa-github size_git"></i>
+              </a>
+            </div>
+          </div>
           {!this.props.loggedIn && (
-            <NavLink to='/loginpage'>
+            <NavLink to="/loginpage">
               <li className="center">
-                <div id='a'>Login</div>
+                <div id="a">Login</div>
               </li>
             </NavLink>
           )}
           {this.props.loggedIn && (
-            <NavLink to='/loginpage' onClick={handleLogOut}>
+            <NavLink to="/loginpage" onClick={handleLogOut}>
               <li className="center">
-                <div id='a'>Log Out</div>
+                <div id="a">Log Out</div>
               </li>
             </NavLink>
           )}
